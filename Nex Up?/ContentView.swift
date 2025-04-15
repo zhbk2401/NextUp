c897dc682562d793797a5c0131af04e5e1c3c7c1
zhbk2401 <zenazybak@gmail.com>
2025-04-15 09:53:42 +0300

    Initial Commit

---
+++ Nex Up?/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nex Up?
+//
+//  Created by ZHBK 01 on 15.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nex Up?/Nex_Up_App.swift
@@ -0,0 +1,17 @@
+//
+//  Nex_Up_App.swift
+//  Nex Up?
+//
+//  Created by ZHBK 01 on 15.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Nex_Up_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
